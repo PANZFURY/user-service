2b5be171f8eac2cbf7b551c48dbca6ab575b7446
Mikhail <mikhailveqluw@gmail.com>
2025-11-07 03:04:34 +0300

    add user repository template

---
+++ src/main/java/practical/task/userservice/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package practical.task.userservice.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import practical.task.userservice.models.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByEmail(String email);
+
+    Page<User> findAll(Specification<User> spec, Pageable pageable);
+
+    void updateActiveStatus(boolean active);
+}
