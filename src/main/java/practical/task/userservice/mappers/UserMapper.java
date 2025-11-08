363d33d0ece62032a602210447e56978751d7dfc
Mikhail <mikhailveqluw@gmail.com>
2025-11-08 20:03:02 +0300

    add UserMapper

---
+++ src/main/java/practical/task/userservice/mappers/UserMapper.java
@@ -0,0 +1,17 @@
+package practical.task.userservice.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import practical.task.userservice.dtos.UserCreateDto;
+import practical.task.userservice.models.User;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "active", defaultValue = "true")
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    User fromUserCreateDto(UserCreateDto userCreateDto);
+}
