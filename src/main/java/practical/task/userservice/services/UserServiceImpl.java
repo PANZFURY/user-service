fc133d51531330feb6babd6c30871dbabb0add40
Mikhail <mikhailveqluw@gmail.com>
2025-11-09 04:44:59 +0300

    implement getAll method in UserServiceImpl

--- src/main/java/practical/task/userservice/services/UserServiceImpl.java
+++ src/main/java/practical/task/userservice/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package practical.task.userservice.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -7,14 +8,27 @@ import org.springframework.stereotype.Service;
 import practical.task.userservice.dtos.UserResponse;
 import practical.task.userservice.dtos.requests.UserCreateDto;
 import practical.task.userservice.dtos.requests.UserUpdateDto;
+import practical.task.userservice.mappers.UserMapper;
 import practical.task.userservice.models.User;
+import practical.task.userservice.repositories.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
     @Override
     public Page<UserResponse> getAll(Specification<User> spec, Pageable pageable) {
-        return null;
+        return userRepository
+                .findAll(spec, pageable)
+                .map(userMapper::toUserResponse);
     }
 
     @Override
