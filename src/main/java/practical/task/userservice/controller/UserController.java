b54c71624d232b968e2fe33db6c4e2cbe9a0e23b
Mikhail <mikhailveqluw@gmail.com>
2025-11-11 13:14:19 +0300

    implement getAll method with specification

--- src/main/java/practical/task/userservice/controller/UserController.java
+++ src/main/java/practical/task/userservice/controller/UserController.java
@@ -1,6 +1,9 @@
 package practical.task.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import practical.task.userservice.dto.response.UserResponse;
@@ -20,6 +23,18 @@ public class UserController {
     @GetMapping("/user/get/{id}")
     public ResponseEntity<UserResponse> getOne(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getOneById(id));
+    }
+
+    @GetMapping("/user/get/all")
+    public ResponseEntity<Page<UserResponse>> getAll(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String surname,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        return ResponseEntity.ok(userService.getAll(name, surname, pageable));
     }
 
 }
