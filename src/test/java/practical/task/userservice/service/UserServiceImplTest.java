9dc2dd1dd973eb0d17868e7acd52a2e3f824f1fb
Mikhail <mikhailveqluw@gmail.com>
2025-11-11 22:24:50 +0300

    implement testCreateUser in UserServiceImplTest

--- src/test/java/practical/task/userservice/service/UserServiceImplTest.java
+++ src/test/java/practical/task/userservice/service/UserServiceImplTest.java
@@ -1,11 +1,14 @@
 package practical.task.userservice.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import practical.task.userservice.dto.request.userDto.UserCreateDto;
+import practical.task.userservice.dto.request.userDto.UserUpdateDto;
+import practical.task.userservice.dto.response.UserResponse;
 import practical.task.userservice.mapper.UserMapper;
 import practical.task.userservice.model.User;
 import practical.task.userservice.repository.PaymentCardRepository;
@@ -13,31 +16,69 @@ import practical.task.userservice.repository.UserRepository;
 
 import java.time.LocalDate;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
-public class UserServiceImplTest {
+class UserServiceImplTest {
 
     @Mock
     private UserRepository userRepository;
     @Mock
     private UserMapper userMapper;
-    @Mock
-    private PaymentCardRepository paymentCardRepository;
 
     @InjectMocks
     private UserServiceImpl userService;
 
-    private UserCreateDto userCreateDto;
     private User user;
+    private UserResponse userResponse;
+    private UserCreateDto createDto;
+    private UserUpdateDto updateDto;
 
     @BeforeEach
     void setUp() {
-        userCreateDto = new UserCreateDto("Britney", "Spears", LocalDate.of(1981, 12, 2), "britney@gmail.com");
         user = new User();
+        user.setId(1L);
         user.setName("Britney");
         user.setSurname("Spears");
         user.setBirthDate(LocalDate.of(1981, 12, 2));
         user.setEmail("britney@gmail.com");
+
+        userResponse = new UserResponse(
+                1L,
+                "Britney",
+                "Spears",
+                LocalDate.of(1981, 12, 2),
+                "britney@gmail.com"
+        );
+
+        createDto = new UserCreateDto(
+                "Britney",
+                "Spears",
+                LocalDate.of(1981, 12, 2),
+                "britney@test.com"
+        );
+
+        updateDto = new UserUpdateDto(
+                "Johny",
+                "Depp",
+                LocalDate.of(1990, 1, 1),
+                "johnny@gmail.com"
+        );
     }
 
+    @Test
+    void testCreateUser_success() {
+        when(userMapper.fromUserCreateDto(createDto)).thenReturn(user);
+        when(userRepository.save(user)).thenReturn(user);
+        doReturn(userResponse).when(userMapper).toUserResponse(user);
+
+        UserResponse response = userService.createUser(createDto);
+
+        assertEquals("Britney", response.name());
+        verify(userRepository).save(user);
+        verify(userMapper).fromUserCreateDto(createDto);
+        verify(userMapper).toUserResponse(user);
+    }
 
 }
