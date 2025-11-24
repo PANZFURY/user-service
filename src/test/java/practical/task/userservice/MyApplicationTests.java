c4c37961535cfab3b4e12e97b021e8ad766e6899
Mikhail <mikhailveqluw@gmail.com>
2025-11-24 21:04:49 +0300

    add application-test.yml

--- src/test/java/practical/task/userservice/MyApplicationTests.java
+++ src/test/java/practical/task/userservice/MyApplicationTests.java
@@ -2,8 +2,10 @@ package practical.task.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyApplicationTests {
 
 	@Test
